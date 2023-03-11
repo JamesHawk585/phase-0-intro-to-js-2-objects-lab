41b013a34ce6d84ccfc199dd6dc29c0bd61c6ad6
James Hawk <jph94880@gmail.com>
2023-03-10 18:29:17 -0600

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employee = {
+  name: "James",
+  job: "Coder",
+} 
+
+
+/* employee.name = "John";
+
+console.log(employee.name); */
+
+// Returns a new employee object with the given key and value added
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, { [key]: value });
+}
+
+// Updates the original employee object with the given key and value, and returns the updated object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Returns a new employee object with the given key removed
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = Object.assign({}, employee);
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Removes the given key from the original employee object and returns the modified object
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
